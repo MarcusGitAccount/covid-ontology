b877cd20e7b5d69cc0b98a45fb794512738de65e
Marcus Pop <marcuspopb@gmail.com>
2021-05-21 00:44:27 +0300

    covid owl and dl learner classification

--- ro-covid-vaccine-ontology/parser.py
+++ ro-covid-vaccine-ontology/parser.py
@@ -18,7 +18,6 @@ lut = {
   'CT': 'Constanta_County',
   'CV': 'Covasna_County',
   'CL': 'Calarasi_County',
-  'SB': 'Sibiu_County',
   'SJ': 'Salaj_County',
   'VN': 'Vrancea_County',
   'VL': 'Valcea_County',
@@ -30,6 +29,32 @@ lut = {
   'TR': 'Teleorman_County'
 }
 
+pop = {
+  'Vaslui_County':	395500,
+  'Cluj_County':	691106,
+  'Olt_County':	415530,
+  'Maramures_County':	516562,
+  'Harghita_County':	304969,
+  'Hunedoara_County':	396253,
+  'Ialomita_County':	293352,
+  'Iasi_County':	772348,
+  'Ilfov_County':	999562,
+  'Satu_Mare_County':	329079,
+  'Tulcea_County':	201462,
+  'Constanta_County':	684082,
+  'Covasna_County':	210177,
+  'Calarasi_County':	285050,
+  'Salaj_County':	343347,
+  'Vrancea_County':	340310,
+  'Valcea_County':	355320,
+  'Neamt_County':	470766,
+  'Prahova_County':	762886,
+  'Suceava_County':	634810,
+  'Mures_County':	550846,
+  'Mehedinti_County':	303878,
+  'Teleorman_County':	360178
+}
+
 if __name__ == '__main__':
   path = os.path.join(dirname, 'date_19_mai_la_13_00.json')
 
@@ -37,17 +62,31 @@ if __name__ == '__main__':
     data = json.loads(fd.read())
     counties_cases = data['currentDayStats']['countyInfectionsNumbers']
     counties_incidence = data['currentDayStats']['incidence']
-    rules_cases = []
-    rules_incidence = []
-    
+    counties_vaccinated = data['currentDayStats']['countyVaccinatedPercent']
+    counties_cases_yesterday = data['historicalData']['2021-05-18']['countyInfectionsNumbers']
+
+    rules_cases, rules_incidence, rules_vaccinated, owl_kb = [], [], [], []
+    i = 1
 
     for county in counties_cases.keys():
       if county in lut:
-        cases = f'(attribute-filler {lut[county]}	{counties_cases[county]} has-cases)\n'
+        cases = f'(attribute-filler {lut[county]}	{counties_cases[county] - counties_cases_yesterday[county]} has-cases)\n'
         incidence = f'(attribute-filler {lut[county]}	{counties_incidence[county]} has-incidence)\n'
+        vaccinated = f'(attribute-filler {lut[county]}	{counties_vaccinated[county]} has-vaccinated)\n'
 
         rules_cases.append(cases)
         rules_incidence.append(incidence)
+        rules_vaccinated.append(vaccinated)
+
+        owl = f""" 
+          <County rdf:ID="#{i}">
+            <has-pop rdf:datatype="&xsd;double">{float(pop[lut[county]])}</has-pop>
+            <has-vaccinated rdf:datatype="&xsd;double">{counties_vaccinated[county]}</has-vaccinated>
+          </County>
+        """
+
+        owl_kb.append(owl)
+        i += 1
 
     # print(len(rules), len(lut.keys()))
     assert len(rules_cases) == len(lut.keys())
@@ -55,5 +94,9 @@ if __name__ == '__main__':
     # print(rules)
     with open(os.path.join(dirname, 'counties.racer'), 'w') as fd_counties:
       fd_counties.writelines(rules_cases)
-      fd.writelines('')
+      fd_counties.write('\n')
       fd_counties.writelines(rules_incidence)
+      fd_counties.write('\n')
+      fd_counties.writelines(rules_vaccinated)
+      fd_counties.write('\n')
+      fd_counties.writelines(owl_kb)
